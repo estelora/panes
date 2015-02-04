e513c7e9ae360c2a002b401045369c11f16fb486
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-02-04 00:22:54 -0500

    first commit

---
+++ js/page-manager.js
@@ -0,0 +1,14 @@
+/*Page manager is responsible for changing from one page to another
+in our single page application...*/
+app.pages = app.PageManager = function (rootElement) {
+    return {
+    registerPage: function (name, callback) {
+
+  },
+
+    goTo: function (name, data) {
+
+  }
+
+};
+};
